32288212ff4f8da02074b7c36ca0fd199e7510f5
brian <brian_coder_herrera@hotmail.com>
2024-11-11 20:39:02 -0300

    pequeño cambio

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 import {mongoose} from 'mongoose';
-import {db_config} from '../config.js'
+import {db_config} from '../config.js';
 
 // ${db_config.database}?retryWrites=true&w=majority&appName=Cluster0`;
-const URI = `mongodb+srv://${db_config.user}:${db_config.password}@cluster0.86no8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const URI = `mongodb+srv://${db_config.user}:${db_config.password}@${db_config.database}.86no8.mongodb.net/?retryWrites=true&w=majority&appName=${db_config.database}`;
 
 
 export async function connect() {
